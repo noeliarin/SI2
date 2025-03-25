c9cf7851e3362c8327ad45587a04167f810606ba
noeliarin <noelia.rincon@estudiante.uam.es>
2025-03-25 22:43:14 +0100

    cliente

--- P1-ws-client/votoAppWSClient/tests_views.py
+++ P1-ws-client/votoAppWSClient/tests_views.py
@@ -1,3 +1,4 @@
+# SIIII
 # Common code for client applications
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
