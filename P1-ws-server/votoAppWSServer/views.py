64526f785209733275c3761fab37feb17a427432
noeliarin <noelia.rincon@estudiante.uam.es>
2025-03-26 13:58:46 +0100

    server

--- P1-ws-server/votoAppWSServer/views.py
+++ P1-ws-server/votoAppWSServer/views.py
@@ -75,6 +75,10 @@ class CensoView(APIView):
 class VotoView(APIView):
     """Emisión y eliminación de un voto"""
 
+    def get(self, request):
+        """Evita error 405"""
+        return Response({'message': 'Método GET no implementado en esta API, usa POST.'}, status=status.HTTP_200_OK)
+
     def post(self, request):
         print(f"Datos recibidos: {request.data}")  # Debug
 
